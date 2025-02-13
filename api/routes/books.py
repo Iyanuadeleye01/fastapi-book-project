0a0ca4b5c4d50804a4fe66ea42ccc60a45bf5871
Adeleye Iyanu <148917844+Iyanuadeleye01@users.noreply.github.com>
2025-02-14 00:54:47 +0100

    Update books.py

--- api/routes/books.py
+++ api/routes/books.py
@@ -57,7 +57,7 @@ async def get_book(book_id: int) -> Book:
 
 @router.get("/{book_id}/test", status_code=status.HTTP_200_OK)
 async def get_book(book_id: int):
-    return "iyanu you are mad o are you sure this is workind - SURPRISE!!!"
+    return "What is going on JARE"
 
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
